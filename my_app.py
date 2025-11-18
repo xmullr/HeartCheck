fa932641317fd3124f7651cfd965edcbe9dbe1d0
xmullr <irkutovmax@gmail.com>
2025-11-18 20:43:43 +0700

    f

--- my_app.py
+++ my_app.py
@@ -1,1 +1,16 @@
-#dfdfdfdf
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QPushButton, QWidget, QApplication, QVBoxLayout
+from instr import *
+
+class Main_win(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(win_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+app = QApplication([])
+main_win = Main_win()
+app.exec_()
